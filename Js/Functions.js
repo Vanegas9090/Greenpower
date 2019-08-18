f39166ae7ed586f8582504eeacfed29562f8f565
Mario Vanegas <mario.vanegas9090@gmail.com>
2019-08-17 21:01:24 -0600

    Adding loading gif

--- Js/Functions.js
+++ Js/Functions.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+    $("a").on('click', function (event) {
   
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash != "") {
@@ -14,11 +14,20 @@ $(document).ready(function(){
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body, div').animate({
           scrollTop: $(hash).offset().top
-        }, 1000, function() {
-     
+        }, 1000, function () {
+  
           // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
         });
       } // End if
     });
   });
+
+function loading(evento) {
+    $("#loading").fadeOut();
+    $("#loading").fadeOut("slow");
+    $("#loading").fadeOut(3000, function () {
+        document.getElementById("loading").style.display="none";
+    });
+    //
+}
