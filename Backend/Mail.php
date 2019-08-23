bc49fc4cc2243554c1f4586275428031bee93458
Mario Vanegas <mario.vanegas9090@gmail.com>
2019-08-23 15:12:53 -0600

    Solo faltan emails

--- Backend/Mail.php
+++ Backend/Mail.php
@@ -1,11 +1,53 @@
 <?php
-    $nombre = "Mario Vanegas";
-    $asunto = "Prueba php";
-    $mensaje = $nombre . " Ha dicho Hola mundo";
 
-    if (mail('mariocanario98@gmail.com', $asunto, $mensaje)) {
-        echo "Correo enviado";
-    } else {
-        echo "Error al enviar correo<br>";
-    }
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'Mailer/src/Exception.php';
+require 'Mailer/src/PHPMailer.php';
+require 'Mailer/src/SMTP.php';
+
+$mail = new PHPMailer(TRUE);
+
+try {
+   
+   $mail->setFrom('darth@empire.com', 'Darth Vader');
+   $mail->addAddress('palpatine@empire.com', 'Emperor');
+   $mail->Subject = 'Force';
+   $mail->Body = 'There is a great disturbance in the Force.';
+   
+   /* SMTP parameters. */
+   
+   /* Tells PHPMailer to use SMTP. */
+   $mail->isSMTP();
+   
+   /* SMTP server address. */
+   $mail->Host = 'smtp.empire.com';
+
+   /* Use SMTP authentication. */
+   $mail->SMTPAuth = TRUE;
+   
+   /* Set the encryption system. */
+   $mail->SMTPSecure = 'tls';
+   
+   /* SMTP authentication username. */
+   $mail->Username = 'smtp@empire.com';
+   
+   /* SMTP authentication password. */
+   $mail->Password = 'iamyourfather';
+   
+   /* Set the SMTP port. */
+   $mail->Port = 587;
+   
+   /* Finally send the mail. */
+   $mail->send();
+}
+catch (Exception $e)
+{
+   echo $e->errorMessage(). "error 1";
+}
+catch (\Exception $e)
+{
+   echo $e->getMessage(). "error 2";
+}
+
 ?>
