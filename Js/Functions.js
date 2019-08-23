910742ef810606d04bb00490e4ad8b82df13557f
Mario Vanegas <mario.vanegas9090@gmail.com>
2019-08-22 19:05:20 -0600

    Casi lista

--- Js/Functions.js
+++ Js/Functions.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  $("a").on('click', function (event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash != "") {
@@ -14,8 +14,8 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body, div').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
+      }, 800, function () {
+
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
@@ -80,7 +80,7 @@ function toggleMenu() {
 
 function toggleMenuOutside() {
   if ($('.SlideContent').css('display') == 'block' && $('.MenuLogo2').css('display') == 'block') {
-      // Acción si el elemento no es visible
-      toggleMenu();
+    // Acción si el elemento no es visible
+    toggleMenu();
   }
-} 
+}
